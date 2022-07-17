a1eccd53ea217189e2b88fae9e916d40596ac097
nbazhan <104205406+nbazhan@users.noreply.github.com>
2022-07-17 21:51:59 +0300

    Delete lib/+gpe directory

--- lib/+gpe/@Grid/initXYZ.m
+++
@@ -1,9 +0,0 @@
-function [X, Y, Z] = initXYZ(obj)
-[X, Y, Z] = meshgrid(obj.r, obj.r, obj.rz);
-if obj.GPU
-    X = gpuArray(X);
-    Y = gpuArray(Y);
-    Z = gpuArray(Z);
-end
-end
-
--- lib/+gpe/@Model/addPotentialVertical.m
+++
@@ -1,7 +0,0 @@
-function addPotentialVertical(obj, params)
-params.config = obj.config;
-params.grid = obj.grid;
-Vb = gpe2.PotentialVertical(params);
-obj.Vbs = [obj.Vbs Vb];
-end
-
