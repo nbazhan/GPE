c4210de1c009b147d82719818651334c0c9f6543
nbazhan <104205406+nbazhan@users.noreply.github.com>
2022-07-17 22:04:39 +0300

    Add files via upload

--- example_dynamics.m
+++ example_dynamics.m
@@ -2,34 +2,36 @@
 clear;
 
 % propagate sf cycles each for real time dt
-sf = 20;
+sf = 50;
 dt = 0.005;
 
 % initialize objects: grid, config, model, save, run
 example_init;
 
+[Psi, mu] = run.getITP();
+
 tic;
 t = 0;
 for s = 1 : sf + 1
     % save pictures and data
-    save.savePic(Psi, s, t) ;
-    save.saveData(s, sf, struct('t', t, ...
-                                'mu', mu, ...
-                                'Psi', Psi, ...
+    sv.savePic(Psi, s, t) ;
+    sv.saveData(s, sf, struct('t', t, ...
+                              'mu', mu, ...
+                               'Psi', Psi, ...
                                 'L', model.getAllL(Psi), ...
-                                'Ub', model.getAllUb(t)));
-        
+                                'Ub', model.getAllUb(t), ...
+                                'U1d', model.getU1d(t)));  
     % display information
     disp([num2str(s), '/', num2str(sf), ...
-          ': L = [', num2str(model.getAllL(Psi), '%0.2f'), ...
+          ': L = [', num2str(model.getAllL(Psi)), ...
           '], time: ', num2str(toc, '%0.0f'), ' s']);
         
     % calculate evolution on dt
     if s < sf + 1
-        [Psi, mu] = run.runGPE(Psi, mu, t + dt);
+        [Psi, mu] = run.runGPE(Psi, mu, t, t + dt);
         t = t + dt;
     end
 end
     
 % save illustrations with angular momentum
-save.saveIlls()
+sv.saveIlls()
