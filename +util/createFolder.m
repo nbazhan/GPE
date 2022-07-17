49d0a0f9a53bba68cdd90af049afefc463169056
nbazhan <104205406+nbazhan@users.noreply.github.com>
2022-07-17 22:09:49 +0300

    Delete +util directory

--- +util/createFolder.m
+++
@@ -1,6 +0,0 @@
-function createFolder(folder)
-% create empty folder
-if ~exist(folder, 'dir')
-    mkdir(folder)
- end
-end
--- +util/myPolylog.m
+++
@@ -1,9 +0,0 @@
-function res = myPolylog(s, z)
-
-N = size(z, 1);
-r = linspace(-1, 1, N);
-res = zeros('like', z);
-for i = 1 : 100
-    res = res + z.^i/i^s;
-end
-
